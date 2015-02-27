d5ce9f64cb8307190aae9901de49ce0dde866270
Jake Smith <jakehockey10@gmail.com>
2015-02-26 22:45:54 -0700

    reverting

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -2,14 +2,13 @@
  * Created by jake on 2/26/15.
  */
 var search = function () {
-    console.log('javascript');
-    //if ( == 'development') {
-    //    $.get('/search/term=food&location=San+Francisco', function (data) {
-    //        console.log(data);
-    //    });
-    //} else {
-        $.get('https://rocky-everglades-5328.herokuapp.com/search/term=food&location=San+Francisco', function (data) {
+    //if (process.env.NODE_ENV == 'development') {
+        $.get('/search/term=food&location=San+Francisco', function (data) {
             console.log(data);
         });
+    //} else {
+    //    $.get('https://rocky-everglades-5328.herokuapp.com/search/term=food&location=San+Francisco', function (data) {
+    //        console.log(data);
+    //    });
     //}
 };
