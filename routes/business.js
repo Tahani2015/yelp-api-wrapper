2cd46abf1d253570b235b9604400f4f383811565
Jake Smith <jakehockey10@gmail.com>
2015-02-27 22:47:53 -0700

    replace spaces with dashes

--- routes/business.js
+++ routes/business.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/:query', function(req, res) {
     var yelp = req.app.locals.yelp;
-    var query = req.params.query;
+    var query = req.params.query.replace(' ', '-');
 	//business function in "yelp" does not require a JSON
     yelp.business(query, function (error, data) {
         if (error) {
